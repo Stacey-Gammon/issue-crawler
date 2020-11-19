bf9782fbc7967819423a6f59a0801b57b4e36edc
Stacey Gammon <gammon@elastic.co>
2020-11-19 09:05:26 -0500

    another fix to the date parsing for env var

--- src/reference_crawler/config.ts
+++ src/reference_crawler/config.ts
@@ -1,5 +1,5 @@
 export const repo = "elastic/kibana";
 
 export function getCheckoutDates() {
-  return process.env.CHECKOUT_DATES ? process.env.CHECKOUT_DATES.split(', ') : [undefined]
+  return process.env.CHECKOUT_DATES ? process.env.CHECKOUT_DATES.split(',').map(d => d.trim()) : [undefined]
 }
